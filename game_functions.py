b73db24b922d74bd12d9b48561153bb50e3a4390
PierreDV <p.verteuil@gmail.com>
2018-11-04 14:18:56 +0100

    refactor key up and key down event checkers into seperate functions

--- game_functions.py
+++ game_functions.py
@@ -6,27 +6,32 @@ def check_events(unicorn_dude):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move unicorn dude to the right.
-                unicorn_dude.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                unicorn_dude.moving_left = True
-            elif event.key == pygame.K_UP:
-                unicorn_dude.moving_up = True
-            elif event.key == pygame.K_DOWN:
-                unicorn_dude.moving_down = True
-
+            check_keydown_events(event, unicorn_dude)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                unicorn_dude.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                unicorn_dude.moving_left = False
-            elif event.key == pygame.K_UP:
-                unicorn_dude.moving_up = False
-            elif event.key == pygame.K_DOWN:
-                unicorn_dude.moving_down = False
+            check_keyup_events(event, unicorn_dude)
+            
+def check_keydown_events(event, unicorn_dude):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        unicorn_dude.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        unicorn_dude.moving_left = True
+    elif event.key == pygame.K_UP:
+        unicorn_dude.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        unicorn_dude.moving_down = True
+
+def check_keyup_events(event, unicorn_dude):
+    """Respond to keyreleases"""
+    if event.key == pygame.K_RIGHT:
+        unicorn_dude.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        unicorn_dude.moving_left = False
+    elif event.key == pygame.K_UP:
+        unicorn_dude.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        unicorn_dude.moving_down = False
 
 def update_screen(ai_settings, screen, unicorn_dude):
     """Update images on the screen and flip to the new screen."""
